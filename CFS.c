abb0fa786ecb258b0a9b3f1c1111f53b9f9da363
Vivihacks <53351333+Vivihacks@users.noreply.github.com>
2024-05-02 15:44:41 -0400

    Update CFS.c

--- CFS.c
+++ CFS.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to CFS Exploits\n\n");
+
+    while (1) {
+        printf("1. ARP Poison\n");
+        printf("Choose Target IP:\n");
+        char targetIP[16];
+        scanf("%s", targetIP);
+
+        printf("Enter Target Port:\n");
+        int targetPort;
+        scanf("%d", &targetPort);
+
+        printf("Connection established with %s:%d. What do you want to do?\n", targetIP, targetPort);
+
+        int choice;
+        printf("1. Grab Cookies\n");
+        printf("2. Steal Files\n");
+        printf("3. Kill\n");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Gathering cookies from Target...\n");
+                printf("Cookies collected\n\n");
+                break;
+            case 2:
+                printf("Files are being copied and locked on device\n\n");
+                break;
+            case 3:
+                printf("Nulling them\n\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n\n");
+        }
+    }
+
+    return 0;
+}
 
